852c31f0d5e60b1ba1c15e62de6187bfa301e3c6
Madeleine <mahdieh.moghadasi@gmail.com>
2021-10-25 22:43:01 -0400

    react state

---
+++ src/components/TextRepeaterButton.js
@@ -0,0 +1,19 @@
+import { useState } from "react";
+
+function TextRepeaterButton () {
+    const [repetitions, setRepetitions] = useState(1);
+
+    const handleClick = () => setRepetitions(repetitions+1)
+
+    const textArray = [];
+    for (let i= 0 ; i <repetitions; i++){
+        textArray.push(<span keys={i} > I like this text!</span>)
+    }
+    return (
+        <button onClick={handleClick} className="TextRepeaterButton">
+            {textArray}
+        </button>
+    );
+}
+
+export default TextRepeaterButton;
